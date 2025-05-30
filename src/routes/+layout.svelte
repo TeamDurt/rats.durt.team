dc13766c56d2229f0a64131ab1c081d649ce4bec
Nikonell <rozhkovn08@gmail.com>
2025-05-30 15:43:32 +0700

    feat: svelte 5

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,12 @@
     import "$lib/assets/css/fonts.css";
     import "$lib/assets/css/globals.css";
     import Header from "$lib/components/Header.svelte";
+
+    interface Props {
+        children?: import('svelte').Snippet;
+    }
+
+    let { children }: Props = $props();
 </script>
 
 <svelte:head>
@@ -12,6 +18,6 @@
 <div class="flex flex-col items-center w-screen min-h-screen px-8">
     <div class="w-full max-w-[1200px] h-full flex flex-col">
         <Header />
-        <slot />
+        {@render children?.()}
     </div>
 </div>
