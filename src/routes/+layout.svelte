01e0e18b7c6c640de45e306516cbb008701d1e73
Nikonell <rozhkovn08@gmail.com>
2025-05-30 15:01:38 +0700

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import "$lib/assets/css/fonts.css";
+    import "$lib/assets/css/globals.css";
+</script>
+
+<svelte:head>
+    <title>Ratsmania</title>
+    <meta name="description" content="Мир меняется. Ты либо часть процесса, либо следующая жертва.">
+</svelte:head>
+
+<slot />
