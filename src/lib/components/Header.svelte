cb4dfe544f4be85ae0293b5e2fa583f4e4840798
Nikonell <rozhkovn08@gmail.com>
2025-05-30 15:40:14 +0700

    feat: static header

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { page } from "$app/state";
+
+    const navItems = [
+        { path: '/', label: 'главная' },
+        { path: '/hints', label: 'подсказки' },
+        { path: '/about', label: 'об ивенте' }
+    ];
+</script>
+
+<div class="w-full h-fit flex flex-row items-center py-4 gap-16">
+    <a href="/" class="text-primary font-bold text-3xl">Ratsmania</a>
+    {#each navItems as { path, label }}
+        <a
+            href={path}
+            class="font-light text-xl"
+            class:text-heading={page.url.pathname === path}
+            class:underline={page.url.pathname === path}
+        >
+            {label}
+        </a>
+    {/each}
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import "$lib/assets/css/fonts.css";
     import "$lib/assets/css/globals.css";
+    import Header from "$lib/components/Header.svelte";
 </script>
 
 <svelte:head>
@@ -8,4 +9,9 @@
     <meta name="description" content="Мир меняется. Ты либо часть процесса, либо следующая жертва.">
 </svelte:head>
 
-<slot />
+<div class="flex flex-col items-center w-screen min-h-screen px-8">
+    <div class="w-full max-w-[1200px] h-full flex flex-col">
+        <Header />
+        <slot />
+    </div>
+</div>
