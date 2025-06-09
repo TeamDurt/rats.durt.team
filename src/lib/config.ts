a75b0cbf7319ad4df69cb4093cbeff963bddae51
Nikonell <rozhkovn08@gmail.com>
2025-06-10 06:08:27 +0700

    initial config

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,7 +4,7 @@ const COLORS = {
     heading: "#ffffff",
 };
 
-export const redirectToWelcome = false;
+export const redirectToWelcome = true;
 
 export const relicsOpened = {
     musicDisk: false,
@@ -17,17 +17,17 @@ export const relicsOpened = {
 type ProgressStep = { type: "point", status: "empty" | "point" | "completed", color: string } | { type: "line", progress: number };
 
 export const progressSteps: ProgressStep[] = [
-    { type: "point", status: "completed", color: COLORS.heading },
-    { type: "line", progress: 100 },
-    { type: "point", status: "point", color: COLORS.primary },
-    { type: "line", progress: 30 },
+    { type: "point", status: "point", color: COLORS.heading },
+    { type: "line", progress: 0 },
+    { type: "point", status: "empty", color: COLORS.paragraph },
+    { type: "line", progress: 0 },
     { type: "point", status: "empty", color: COLORS.paragraph },
     { type: "line", progress: 0 },
     { type: "point", status: "empty", color: COLORS.paragraph },
 ];
 
-export const currentStep: "waiting" | "pyramid" | "labyrinth" | "final" = "pyramid";
-export const stepNumber = 1;
+export const currentStep: "waiting" | "pyramid" | "labyrinth" | "final" = "waiting";
+export const stepNumber = 0;
 export const stepOffset = 96 + stepNumber * 336;
 
 export const dungeonNotesOpened = {
